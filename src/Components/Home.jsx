ef404081dcec5c90c6a439321e599ca3bb3050ec
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-08-23 07:41:43 -0700

    created components folder with files

---
+++ src/Components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className="mainConent">
+            <h1 className="hookup-text">Your one-stop shop for all things tech.</h1>
+        </div>
+    )
+}
