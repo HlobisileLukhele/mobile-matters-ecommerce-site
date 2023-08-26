aa1dcfa24cde27e8f314745333f21607b09b8779
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-08-26 07:43:56 -0700

    changed the h1 and added an image

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,11 +3,11 @@
 import React from "react";
 
 const Home = () => (
-  <div>
-    <h1 className="title is-1">This is the Home Page</h1>
-    <p>
-     Welcome to home page 
-    </p>
+  <div className="Container-fluid">
+
+    <h1 className="title is-1 text-center mt-4 mb-4">Your One Stop Tech Store</h1>
+    <img src="images/apple.png"  className="img-fluid" alt="apple iphones front"/>
+    
   </div>
 );
 
