05a32389a33c6f3fdd74fbc6c1f37c0e6c905346
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-08-26 07:42:42 -0700

    imported & rendere a wishlist icon

--- src/Components/MyNavbar.jsx
+++ src/Components/MyNavbar.jsx
@@ -1,46 +1,74 @@
-import React from 'react';
-import { Outlet, Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Outlet, Link } from 'react-router-dom';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const MyNavbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const handleNavToggle = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
-          <Link to="/" className="navbar-brand">Mobile Matters</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <Link to="/" className="navbar-brand d-flex justify-content-start">
+            Mobile Matters
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            onClick={handleNavToggle}
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div
+            className={`collapse navbar-collapse ${isNavOpen ? 'show' : ''}`}
+            id="navbarNav"
+          >
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
               </li>
               <li className="nav-item">
-                <Link to="/About" className="nav-link">About</Link>
+                <Link to="/About" className="nav-link">
+                  About
+                </Link>
               </li>
               <li className="nav-item">
-                <Link to="/services" className="nav-link">Services</Link>
+                <Link to="/services" className="nav-link">
+                  Services
+                </Link>
               </li>
               <li className="nav-item">
-                <Link to="/contact" className="nav-link">Contact</Link>
+                <Link to="/contact" className="nav-link">
+                  Contact
+                </Link>
               </li>
               <li className="nav-item">
-                <Link to="/shop" className="nav-link">Shop</Link>
+                <Link to="/shop" className="nav-link">
+                  Shop
+                </Link>
               </li>
             </ul>
           </div>
           <div className="d-flex justify-content-end">
             <AccountCircleIcon />
+            <FavoriteIcon />
             <AddShoppingCartIcon />
+           
           </div>
         </div>
       </nav>
 
       <Outlet />
     </>
-  )
+  );
 };
 
 export default MyNavbar;
