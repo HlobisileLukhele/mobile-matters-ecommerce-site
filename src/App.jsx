c067f62b65668cef19f70722c0f659b7e18cf678
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-08-26 06:57:51 -0700

    imported and rendered the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,31 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MyNavbar from "./Components/MyNavbar";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Services from "./Components/Services"
+import Shop from "./Components/Shop";
 
+
+export default function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MyNavbar />}>
+            <Route index element={<Home />} />
+            <Route path="About" element= {<About/>} />
+            <Route path="Services" element={<Services/>} />
+            <Route path="Contact" element={<Contact />} />
+            <Route path="sHOP" element={<Shop/>} /> 
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
