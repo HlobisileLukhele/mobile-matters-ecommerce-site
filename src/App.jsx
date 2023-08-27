0763feac2231c1d712dc7bd6ccd9ba302979b2a6
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-08-27 15:13:29 -0700

    imported the icons, buttom nav bar and rendered it

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import  './App.css'
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import MyNavbar from "./Components/MyNavbar";
 import Home from "./Components/Home";
 import About from "./Components/About";
@@ -9,6 +9,12 @@ import Contact from "./Components/Contact";
 import Services from "./Components/Services"
 import Shop from "./Components/Shop";
 import Profile from "./Components/Profile";
+import WishList from "./Components/WishList"
+import Cart from './Components/Cart';
+import TrackOrder from './Components/TrackOrder';
+import BottomNavigationBar from './Components/BottomNavigationBar';
+
+
 export default function App() {
   return (
     <div className="App">
@@ -20,9 +26,13 @@ export default function App() {
             <Route path="Services" element={<Services/>} />
             <Route path="Contact" element={<Contact />} />
             <Route path="sHOP" element={<Shop/>} />
-            <Route path="Profile" element ={<Profile/>} />
+            <Route path="WishList" element ={<WishList/>} />
+            <Route path="/TrackOrder" element={<TrackOrder />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/cart" element={<Cart />} />
           </Route>
         </Routes>
+        <BottomNavigationBar />
       </BrowserRouter>
     </div>
   );
