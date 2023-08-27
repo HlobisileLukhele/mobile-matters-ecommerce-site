c105786ae99502c0c6bcbada5c9e83a1c22dbe0b
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-08-27 15:11:29 -0700

    created Bottom nav bar linked the icons

---
+++ src/Components/BottomNavigationBar.jsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+
+const BottomNavigationBar = () => {
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <div className="container-fluid bottom-navbar .d-xxl-none .d-xl-none .d-lg-none .d-md-none">
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <Link to="/profile" className="nav-link">
+          <BottomNavigationAction label="Profile" className="bottom-icon" icon={<AccountCircleIcon />} />
+       </Link>
+
+       <Link to="/WishList" className="nav-link">
+          <BottomNavigationAction label="Wishlist" className="bottom-icon" icon={<FavoriteIcon />} />
+       </Link>
+
+       <Link to="/WishList" className="nav-link">
+       <BottomNavigationAction label="Cart" className="bottom-icon" icon={<AddShoppingCartIcon />} />
+       </Link>
+
+       <Link to="/TrackOrder" className="nav-link">
+       <BottomNavigationAction label="Track Order" className="bottom-icon" icon={<LocalShippingIcon />} />
+       </Link>
+      </BottomNavigation>
+    </Box>
+    </div>
+  );
+}
+
+export default BottomNavigationBar;
