4d9a059f6a8ad80f3e2d336606b666aeebe5ff5d
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-08-27 07:11:49 -0700

    reduced the image size

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,8 +5,8 @@ import React from "react";
 const Home = () => (
   <div className="Container-fluid">
 
-    <h1 className="title is-1 text-center mt-4 mb-4">Your One Stop Tech Store</h1>
-    <img src="images/apple.png"  className="img-fluid" alt="apple iphones front"/>
+    <h1 className="title is-1 text-center mt-4 mb-2">Your One Stop Tech Store</h1>
+    <img src="images/apple.png" className="img-fluid landing-page-img mb-4" alt="apple iphones front"/>
     
   </div>
 );
