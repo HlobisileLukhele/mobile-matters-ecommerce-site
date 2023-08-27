f20d155070dd7c8cdb17a1ac9f31ff5aae5c80e5
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-08-27 07:56:40 -0700

    renamed the heading

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const Contact = () => (
   <div>
-    <h1 className="title is-1">This is the Home Page</h1>
+    <h1 className="title is-1">Contact us </h1>
     <p>
-     Welcome to home page 
+    
     </p>
   </div>
 );
