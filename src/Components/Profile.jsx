85f511a678f59482976c7c625df0900406a81a9a
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-08-27 06:02:30 -0700

    created a user login component

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+const Profile = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Here you can add your authentication logic.
+    // For now, let's just display a message indicating successful login.
+    alert('Logged in successfully!');
+  };
+
+  return (
+    <div className="profile-container">
+      <h1>Login to Your Account</h1>
+      <form onSubmit={handleLogin}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control"
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Profile;
