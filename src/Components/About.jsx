a2cf8ad7a4c71ce280343f72ce3ec4230e34b6f9
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-08-29 08:58:11 -0700

    added about desription

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 
 const About = () => (
-  <div>
-    <h1 className="title is-1">About Us</h1>
-    <p>
-     Hello, tell me about you
+  <div className="Container-fluid justify-content-center">
+    <h1 className="title is-1 mb-4 mt-4 text-center">About Us</h1>
+    <p className="about-p1 text-center my-4 mx-4">
+     At Mobile Matters, our journey began with a shared passion for technology and a deep understanding 
+     of how smartphones are more than just gadgets, they're integral to our lives. We're not just a company; 
+     we're your tech-savvy companions on this incredible digital adventure.
+    
+     With every phone we offer, we're not just delivering a piece of hardware; we're putting the world in your pocket,
+     With our personalised accessories curated to match your style.
     </p>
+    <p className="about-p2 text-center my-4 mx-4">
+     But wait, there's more! We're not just about the products; we're about your experience. 
+     Our website services aren't just lines of code, they're pathways to seamless digital interactions.
+     We're here to bridge the gap between your imagination and the online world, ensuring your online presence reflects your uniqueness.
+
+     With every product, every accessory, and every service, we're fueling your journey towards a more
+     connected and vibrant world. Because in this age of technology, it's not just devices we're providing...
+    </p>
+    <h5 className="hook-up text-center my-4 mx-4">
+     "it's the tools to shape your digital destiny."
+    </h5>
   </div>
 );
 
