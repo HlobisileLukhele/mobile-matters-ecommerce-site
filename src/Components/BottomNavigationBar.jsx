84c59da8698aab3ad1bcfd24a556482bf954025e
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-08-29 13:45:52 -0700

    removed the cart icon

--- src/Components/BottomNavigationBar.jsx
+++ src/Components/BottomNavigationBar.jsx
@@ -5,16 +5,16 @@ import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 import CategoryIcon from '@mui/icons-material/Category';
+import FindInPageIcon from '@mui/icons-material/FindInPage';
 
 const BottomNavigationBar = () => {
   const [value, setValue] = React.useState(0);
 
   return (
     <div className="container-fluid bottom-navbar .d-xxl-none .d-xl-none .d-lg-none .d-md-none">
-    <Box sx={{ width: 500 }}>
+    <Box sx={{ width:600, height:500 }}>
       <BottomNavigation
         showLabels
         value={value}
@@ -23,23 +23,26 @@ const BottomNavigationBar = () => {
         }}
       >
         <Link to="/profile" className="nav-link">
-          <BottomNavigationAction label="Profile" className="bottom-icon" icon={<AccountCircleIcon />} />
+          <BottomNavigationAction label="Acount" className="bottom-icon" icon={<AccountCircleIcon />} />
        </Link>
 
        <Link to="/WishList" className="nav-link">
           <BottomNavigationAction label="Wishlist" className="bottom-icon" icon={<FavoriteIcon />} />
        </Link>
 
-       <Link to="/Cart" className="nav-link">
-       <BottomNavigationAction label="Cart" className="bottom-icon" icon={<AddShoppingCartIcon />} />
+       <Link to="/TrackOrder" className="nav-link">
+       <BottomNavigationAction label="Category" className="bottom-icon" icon={<CategoryIcon />} />
        </Link>
 
        <Link to="/TrackOrder" className="nav-link">
        <BottomNavigationAction label="Track Order" className="bottom-icon" icon={<LocalShippingIcon />} />
        </Link>
+
        <Link to="/TrackOrder" className="nav-link">
-       <BottomNavigationAction label="Track Order" className="bottom-icon" icon={<CategoryIcon />} />
+       <BottomNavigationAction label="Search product" className="bottom-icon" icon={<FindInPageIcon />} />
        </Link>
+       
+       
       </BottomNavigation>
     </Box>
     </div>
