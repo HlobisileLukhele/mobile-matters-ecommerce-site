517c65f88243478ea01ed75cd1bb643039e34796
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-09-13 14:38:34 -0700

    imported useQuery

---
+++ src/Components/ProductsList.jsx
@@ -0,0 +1,27 @@
+import {useQuery} from '@apollo/client'
+import Products from "./Products.js"
+import { GET_ALL_PRODUCTS } from './graphqlQueries.jsx'
+
+function ProductsList () {
+    const {loading, error, data} = useQuery(GET_ALL_PRODUCTS)
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>{error.message}</p>;
+    if (data) return <p>{data}</p>
+ 
+    return (
+        <div>
+            {Products.map(product => (
+                <div key={product.id}>
+                <h6>{product.name}</h6>
+                <img src={product.image} alt ={product.name} />
+                <p>{product.description}</p>
+                <p>Price : ${product.price}</p>    
+                </div>
+            ))}
+        </div>
+    );
+       
+
+}
+export default ProductsList;
