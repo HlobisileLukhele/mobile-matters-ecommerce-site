b0400df40e3f9d53603a196335319b1b755ca55c
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-09-18 12:02:34 -0700

    created a web design card

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 
-const About = () => (
+const Services = () => (
+
   <div className="Container-fluid">
     <h1 className="services-heading mb-4mt-4">Website Services</h1>
+    <img src="images/Landing page.png" alt ="web servicees pages" className="mb-4mt-4"/>
     
+  <div class="container text-center">
+  <div class="row">
+    <div class="col-sm">Website Design
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <h5 class="card-title bg-grey">Standard</h5>
+    <p class="card-text">Once-off</p>
+    <h3 class="card-title bg-grey">R2999</h3>
+    <p class="card-text">Some  bulk of .</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Website designa and Development</li>
+    <li class="list-group-item">Responsive and Mobile Friendly</li>
+    <li class="list-group-item">SEO optomized</li>
+    <li class="list-group-item">Free .co.za domain</li>
+
+  </ul>
+</div>
+    </div>
+    <div class="col-sm">e-Commerce </div>
+    <div class="col-sm">e-Mail Hosting </div>
+  </div>
+</div>
+
   </div>
 );
 
-export default About;
+export default Services;
