f4ba4b5b2e6d805dd42962af3b9473aa6d3b58ee
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-09-22 11:37:59 -0700

    imported footer and rendered it

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ContactForm from "./ContactForm"
+import Footer from "./Footer"
 
 const Contact = () => (
   <div>  
     <ContactForm /> 
+    <Footer />
   </div>
 );
 
