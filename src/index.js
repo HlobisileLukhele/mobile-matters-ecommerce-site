d6534b1d8c0b7cfdad8f119cb59107dea6217c66
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-09-30 09:46:02 -0700

    imported apollo provider and included uri link

--- src/index.js
+++ src/index.js
@@ -3,29 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { ApolloServer, gql } from 'apollo-server-express';
-import { typeDefs } from './Schema/type-defs'; 
-import { resolvers } from './Schema/resolvers'; 
+import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider } from '@apollo/client';
 
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000',
-  cache: new InMemoryCache(),
+ link: new HttpLink({
+    uri: 'https://localhost:3000',
+ }),
+ cache: new InMemoryCache(),
 });
 
+    ReactDOM.render(
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>,
+        document.getElementById('root')
+    );
 
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
-);
-
-
-console.log('GraphQL server running at http://localhost:4000/graphql');
+export default App ;
