c8e8bc367e3a6f7f982c914a3a7ffe9d57ded0d5
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-11-22 16:10:37 +0200

    removed the graphql server

--- src/index.js
+++ src/index.js
@@ -3,20 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
-import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider } from '@apollo/client';
-
-
-const client = new ApolloClient({
- link: new HttpLink({
-    uri: 'https://localhost:3000',
- }),
- cache: new InMemoryCache(),
-});
 
     ReactDOM.render(
-        <ApolloProvider client={client}>
-            <App />
-        </ApolloProvider>,
+            <App /> ,
         document.getElementById('root')
     );
 
