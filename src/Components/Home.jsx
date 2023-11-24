3efb8c3a72205835708d381cb6fa3873b27e42d0
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-11-24 07:13:43 +0200

    increaed margin top and bottom

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Home = () => (
-  <div className="Container-fluid">
+  <div className="Container-fluid ">
 
-    <h1 className="title is-1 text-center mt-4 mb-2">Your One Stop Tech Store</h1>
-    <img src="images/apple.png" className="img-fluid landing-page-img mb-4" alt="apple iphones front"/>
+    <h1 className="title is-1 text-center mt-6 mb-6">Your One Stop Tech Store</h1>
+    <img src="images/apple.png" className="img-fluid landing-page-img mb-4 align-items-center" alt="apple iphones front"/>
     
   </div>
 );
