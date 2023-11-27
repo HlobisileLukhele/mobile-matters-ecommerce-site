f7e63296e9e4747e230b8f94ae3c7676910ea1cf
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-11-27 23:54:53 +0200

    included a input search with a button

--- src/Components/SearchItem.jsx
+++ src/Components/SearchItem.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const SearchItem = () => {
     return (
         <div>
-            <h2>Search Item</h2>
+        <h2>Search Item</h2>
+      <input
+        type="text ml-2"
+        placeholder="Search Item "
+        value={orderNumber}
+        onChange={(e) => setOrderNumber(e.target.value)}
+      />
+      <button className="btn btn-secondary mr-2">Track</button>
         </div>
     )
 }
