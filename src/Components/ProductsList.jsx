767153bb75a5efd9d79cf070d729a7d5ea1af72c
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-11-27 23:47:41 +0200

    chnage the name heading to paragraph

--- src/Components/ProductsList.jsx
+++ src/Components/ProductsList.jsx
@@ -9,9 +9,9 @@ export default  function ProductsList () {
           {ProductsData.map((product) => (
             <div key={product.id} className="col-lg-3 col-md-6 col-sm-6 mb-4">
               <div className="card bg-gray-500">
-                <img src={product.image} alt={product.name} className="card-img-top" />
+                <img src={product.image} alt={product.name} className="card-img-top " />
                 <div className="card-body">
-                  <h5 className="card-title">{product.name}</h5>
+                  <p className="card-title">{product.name}</p>
                   <p className="card-text">Price: R{product.price}</p>
                 </div>
               </div>
