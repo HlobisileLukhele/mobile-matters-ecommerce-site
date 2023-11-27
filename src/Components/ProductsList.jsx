b2022dd4aa86bc1cfdb7448cb92ccb6f504a1289
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-11-27 23:36:51 +0200

    chnaged background color to gray

--- src/Components/ProductsList.jsx
+++ src/Components/ProductsList.jsx
@@ -4,14 +4,20 @@ import ProductsData from "./ProductsData.js";
 export default  function ProductsList () {
     const Products = ProductsData;
     return (
-       <div>
-       {ProductsData.map((product) =>(
-        <div key={product.id}>
-        <h2>{product.name}</h2>
-        <img src={product.image} alt={product.name} />
-        <span>Price: ${product.price}</span>
+        <div className="container mb-6 mt-5 items-center">
+        <div className="row">
+          {ProductsData.map((product) => (
+            <div key={product.id} className="col-lg-3 col-md-6 col-sm-6 mb-4">
+              <div className="card bg-gray-500">
+                <img src={product.image} alt={product.name} className="card-img-top" />
+                <div className="card-body">
+                  <h5 className="card-title">{product.name}</h5>
+                  <p className="card-text">Price: R{product.price}</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-       ))}
-       </div>
+      </div>
     );  
 }
