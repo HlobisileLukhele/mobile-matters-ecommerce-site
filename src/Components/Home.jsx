8e9bbe39bc50fc768d9d9df5a475c71b18c8f29e
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-11-27 21:43:39 +0200

    remove the spaces

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,10 +2,8 @@ import React from "react";
 
 const Home = () => (
   <div className="Container-fluid ">
-
     <h1 className="title is-1 text-center mt-6 mb-6">Your One Stop Tech Store</h1>
     <img src="images/apple.png" className="img-fluid landing-page-img mb-4 align-items-center" alt="apple iphones front"/>
-    
   </div>
 );
 
