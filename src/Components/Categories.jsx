3e98c56b8f1b4d77cdd506978361fca0d01711cd
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-11-28 21:19:38 +0200

    created categories with banner images

---
+++ src/Components/Categories.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Categories = () => {
+    return (
+<div className="container mt-4">
+<div class="row mt-4">
+  <div class="col-6 col-sm-2 mt-4 mb-4">
+  <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="images/Products/Accessories-Banner.png" alt="Card image cap" />
+  </div>
+  </div>
+  </div>
+  <div class="col-6 col-sm-2 mt-4 mb-4">
+  <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="images/Products/iphones-banner.png" alt="Card image cap" />
+  </div>
+  </div>
+  </div>
+
+  <div class="col-6 col-sm-2">
+  <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="images/Products/Android-Banner.png" alt="Card image cap" />
+  </div>
+  </div>
+  </div>
+  <div class="col-6 col-sm-2">
+  <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="images/Products/Watches-Banner.png" alt="Card image cap" />
+  </div>
+  </div>
+  </div>
+</div> 
+    </div>
+    );
+} 
+
+export default Categories;
