66c2b61ecd7d4341c81e36af0de3ccaebc81a7b1
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-11-29 14:39:11 +0200

    imported and rendered the footer and categories components

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Categories from './Categories';
+import Footer from './Footer'
 
 const Home = () => (
-  <div className="Container-fluid ">
+  <div className="Container-fluid mb-4">
+  <div>
     <h1 className="title is-1 text-center mt-6 mb-6">Your One Stop Tech Store</h1>
     <img src="images/apple.png" className="img-fluid landing-page-img mb-4 align-items-center" alt="apple iphones front"/>
+  </div>
+    <Categories />
+    <Footer />
   </div>
 );
 
