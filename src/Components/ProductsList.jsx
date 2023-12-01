98fd5c3ec3d42814f4a215865e146eefd530c63a
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-12-01 21:43:51 +0200

    change column size to 2 on small screen

--- src/Components/ProductsList.jsx
+++ src/Components/ProductsList.jsx
@@ -7,9 +7,9 @@ export default  function ProductsList () {
         <div className="container mb-6 mt-5 items-center">
         <div className="row">
           {ProductsData.map((product) => (
-            <div key={product.id} className="col-lg-3 col-md-6 col-sm-6 mb-4">
+            <div key={product.id} className="col-lg-3 col-md-6 col-sm-2 mb-4">
               <div className="card product-card">
-                <img src={product.image} alt={product.name} className="card-img-top max-h-10" />
+                <img src={product.image} alt={product.name} className="card-img-top max-h-10 max-w-sm" />
                 <div className="card-body">
                   <p className="card-title">{product.name}</p>
                   <p className="card-text text-black">Price: R{product.price}</p>
