742909edc1be678cfa9397ce7aa42273a4ef13c5
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-12-01 13:13:27 +0200

    added margin bottom

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -2,31 +2,30 @@ import React from 'react';
 
 const Categories = () => {
     return (
-<div className="container mt-4">
+<div className="container mt-4 mb-4">
 <div class="row mt-4">
-  <div class="col-6 col-sm-2 mt-4 mb-4">
+  <div class="col-6 col-sm-1 mt-4 mb-4">
   <div class="card-deck">
   <div class="card">
     <img class="card-img-top" src="images/Products/Accessories-Banner.png" alt="Card image cap" />
   </div>
   </div>
   </div>
-  <div class="col-6 col-sm-2 mt-4 mb-4">
+  <div class="col-6 col-sm-1 mt-4 mb-4">
   <div class="card-deck">
   <div class="card">
     <img class="card-img-top" src="images/Products/iphones-banner.png" alt="Card image cap" />
   </div>
   </div>
   </div>
-
-  <div class="col-6 col-sm-2">
+  <div class="col-6 col-sm-1">
   <div class="card-deck">
   <div class="card">
     <img class="card-img-top" src="images/Products/Android-Banner.png" alt="Card image cap" />
   </div>
   </div>
   </div>
-  <div class="col-6 col-sm-2">
+  <div class="col-6 col-sm-1 ">
   <div class="card-deck">
   <div class="card">
     <img class="card-img-top" src="images/Products/Watches-Banner.png" alt="Card image cap" />
