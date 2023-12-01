d1a7802435a7a996a7c23b52da2fd8f1e4a77efc
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-12-01 21:37:13 +0200

    adjusted the colums to two per row on desktop

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -4,28 +4,28 @@ const Categories = () => {
     return (
 <div className="container mt-4 mb-4">
 <div class="row mt-4">
-  <div class="col-6 col-sm-1 mt-4 mb-4">
+  <div class="col-6 col-sm-6 col-md-6 mt-4 mb-4">
   <div class="card-deck">
   <div class="card">
     <img class="card-img-top" src="images/Products/Accessories-Banner.png" alt="Card image cap" />
   </div>
   </div>
   </div>
-  <div class="col-6 col-sm-1 mt-4 mb-4">
+  <div class="col-6 col-sm-6 col-md-6 mt-4 mb-4">
   <div class="card-deck">
   <div class="card">
     <img class="card-img-top" src="images/Products/iphones-banner.png" alt="Card image cap" />
   </div>
   </div>
   </div>
-  <div class="col-6 col-sm-1">
+  <div class="col-6 col-sm-6 col-md-6">
   <div class="card-deck">
   <div class="card">
     <img class="card-img-top" src="images/Products/Android-Banner.png" alt="Card image cap" />
   </div>
   </div>
   </div>
-  <div class="col-6 col-sm-1 ">
+  <div class="col-6 col-sm-6 col-md-6">
   <div class="card-deck">
   <div class="card">
     <img class="card-img-top" src="images/Products/Watches-Banner.png" alt="Card image cap" />
