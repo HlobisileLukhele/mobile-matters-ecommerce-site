7312e691d1cf1e419d767e4fb821aef7ebb1827d
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-12-02 17:00:56 +0200

    made the container fluid and made the column responsive for mobile

--- src/Components/ProductsList.jsx
+++ src/Components/ProductsList.jsx
@@ -4,12 +4,12 @@ import ProductsData from "./ProductsData.js";
 export default  function ProductsList () {
     const Products = ProductsData;
     return (
-        <div className="container mb-6 mt-5 items-center">
+        <div className="container-fluid mb-6 mt-5 items-center">
         <div className="row">
           {ProductsData.map((product) => (
-            <div key={product.id} className="col-lg-3 col-md-6 col-sm-2 mb-4">
+            <div key={product.categoryId} className="col-6 col-lg-3 col-sm-6 col-md-2 mb-4">
               <div className="card product-card">
-                <img src={product.image} alt={product.name} className="card-img-top max-h-10 max-w-sm" />
+                <img src={product.image} alt={product.name} className="image-fluid card-img-top max-h-10 max-w-sm  sm:max-sm: object-cover" />
                 <div className="card-body">
                   <p className="card-title">{product.name}</p>
                   <p className="card-text text-black">Price: R{product.price}</p>
