61f2151aa2cbd25609be747df5627ca78ee04371
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-12-02 17:01:36 +0200

    removed the headings

--- src/Components/WishList.jsx
+++ src/Components/WishList.jsx
@@ -6,8 +6,7 @@ const Wishlist = () => {
       <h1 className="mb4 mt-4">Wishlist</h1>
       <div className="product-list">
         <div>
-          <h2 className="mb4 mt-4">Your Products</h2>
-          <h2 className="mb4 mt-4">Other Products</h2>
+         
         </div>
       </div>
     </div>
