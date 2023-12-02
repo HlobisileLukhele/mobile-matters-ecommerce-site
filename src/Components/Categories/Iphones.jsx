43e3fbd3362365653900def79a0e5d0ea0a2cea6
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-12-02 17:04:24 +0200

    created iphone categeries file

---
+++ src/Components/Categories/Iphones.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ProductsData from 'ProductsData'
+
+const Iphones = () => {
+    const Products = ProductsData;
+    return (
+        <div className="conatiner-fluid items-center mb-4">
+        <div className="row">
+        <div className="col-auto">
+            
+        </div>
+
+        </div>
+        </div>
+
+    )
+    }
+
+export default PhoneFile;
