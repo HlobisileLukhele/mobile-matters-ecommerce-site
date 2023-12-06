2b49755a757a0ebaf2735fe59b5bc3e34f78973a
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-12-06 19:44:54 +0200

    created item file with items to me displayed on the cart page

---
+++ src/Components/Item.jsx
@@ -0,0 +1,23 @@
+import { useDispatch } from "react-redux";
+import { addToCart } from "../Features/cartSlice";
+
+function Item({id,title, image, price}) {
+    const dispatch = useDispatch()
+}
+
+return (
+    <div className="item">
+        <div className="item__info">
+            <p className="item__title">{title}</p>
+            <p className="item__price">
+                <small>R</small>
+                <strong>{price}</strong>
+            </p>
+        </div>
+        <img src={image} alt="item" />
+        <button onClick={() => dispatch(addToCart({
+            id,title,image,price
+        }))
+        }> Add to Cart</button>
+    </div>
+)
