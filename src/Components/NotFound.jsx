71171cdfbb02ec07a06ba81f8161db1827b68e4d
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-12-06 19:32:07 +0200

    created a page not found file

---
+++ src/Components/NotFound.jsx
@@ -0,0 +1,11 @@
+
+const NotFound = () => {
+    return (
+        <div className="not-found">
+        <h1 className="404 text-lg text-center text-black mt-40 mb-4">404</h1>
+        <p className="page-not-found text-gray-800 text-center mt-4 ">Page Not Found</p>
+        </div>
+    )
+}
+
+export default NotFound;
