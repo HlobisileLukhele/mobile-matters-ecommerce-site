787f4c4efd045e17bb90cdda0620347eaff88bf5
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-12-07 23:12:10 +0200

    wrapped the input and button in a form tag and added classnames

--- src/Components/SearchItem.jsx
+++ src/Components/SearchItem.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 const SearchItem = () => {
     return (
         <div className='container mt-5 mb-4'>
-      <input
+              <h2 className="track-order mb-4 mt-4">Search Item</h2>
+        <form className="items-centered text-center form-input">
+        <input
         type="text ml-2"
-        placeholder="SearchItem "
-        className='mr-2 ml-2'
+        placeholder="   enter item here"
+        className=' Search-item mr-4 ml-4 mt-4'
       />
-      <button className="btn btn-secondary ">Search</button>
+      <button className="btn btn-secondary  search-btn">Search</button>
+        </form>
+     
         </div>
     )
 }
