209ea4bc231640060bb4f73c50ee3798e13b11ad
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-12-10 23:29:27 +0200

    no changes made

--- src/Components/MyNavbar.jsx
+++ src/Components/MyNavbar.jsx
@@ -4,8 +4,6 @@ import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
-import CategoryIcon from '@mui/icons-material/LocalShipping';
-
 
 const MyNavbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -14,19 +12,10 @@ const MyNavbar = () => {
     setIsNavOpen(!isNavOpen);
   };
 
-  const toggleLoginForm = () => {
-    setShowLoginForm(!showLoginForm);
-  };
-
-  const [showLoginForm, setShowLoginForm] = useState(false);
-
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
-          <Link to="/" className="navbar-brand justify-content-start! text-sm font-mono">
-            Mobile Matters
-          </Link>
+      <nav className=" d-flex  navbar navbar-expand-lg navbar-light bg-light text-black">
+        <div className="container d-flex">
           <button
             className="navbar-toggler"
             type="button"
@@ -34,11 +23,14 @@ const MyNavbar = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
+          <Link to="/" className="navbar-brand text-md text-black text-sm ml-0 mr-5 text-centered justify-content-center">
+            Mobile Matters
+          </Link>
           <div
             className={`collapse navbar-collapse ${isNavOpen ? 'show' : ''}`}
             id="navbarNav"
           >
-            <ul className="navbar-nav d-flex justify-content-center mr-4">
+            <ul className="navbar-nav d-flex justify-content-left mr-2 text-black">
               <li className="nav-item active">
                 <Link to="/" className="nav-link">
                   Home
@@ -49,7 +41,7 @@ const MyNavbar = () => {
                   Shop
                 </Link>
               </li>
-               <li className="nav-item">
+              <li className="nav-item">
                 <Link to="/services" className="nav-link">
                   Web Services
                 </Link>
@@ -64,37 +56,31 @@ const MyNavbar = () => {
                   Contact
                 </Link>
               </li>
-              
             </ul>
+            <div className="d-flex justify-content-end nav-icons  ">
+              <Link to="./TrackOrder">
+                <LocalShippingIcon
+                  className="nav-icon d-none d-md-block d-lg-block"
+                />
+              </Link>
+              <Link to="./WishList">
+                <FavoriteIcon
+                  className="nav-icon  d-none d-md-block d-lg-block d-md-block d-lg-block"
+                />
+              </Link>
+              <Link to="./CartItem">
+                <AddShoppingCartIcon className="nav-icon" />
+              </Link>
+              <Link to="/profile">
+                <AccountCircleIcon
+                  className="nav-icon d-none d-md-block d-lg-block"
+                  style={{ cursor: 'pointer' }}
+                />
+              </Link>
+            </div>
           </div>
-          <div className="d-flex justify-content-end nav-icons ">
-          <Link to="/profile">
-            <AccountCircleIcon
-             className="nav-icon d-none d-md-block d-lg-block"
-             onClick={toggleLoginForm}
-            style={{ cursor: 'pointer' }}
-              />
-        </Link>
-        <Link to="./WishList">
-          <FavoriteIcon
-           className="nav-icon  d-none d-md-block d-lg-block d-md-block d-lg-block" 
-           />
-        </Link>
-        <Link to="./Cart">
-       <AddShoppingCartIcon 
-       className="nav-icon   d-md-block d-lg-block d-md-block d-lg-block" 
-       />
-       </Link>
-
-       <Link to="./TrackOrder">
-       <LocalShippingIcon 
-       className="nav-icon d-none d-md-block d-lg-block" 
-       />
-       </Link>
-        </div>
         </div>
       </nav>
-
       <Outlet />
     </>
   );
