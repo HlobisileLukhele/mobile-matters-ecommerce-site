12ed7cf7a8401d66924a8781f0fa0475f8a8aacc
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-04-11 02:47:03 +0200

    deleted firebase user authentication logic

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,45 +1,12 @@
-import React, { useState } from 'react';
-import { AuthErrorCodes, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React from 'react';
 
 function LoginForm() {
-  const [input, setInput] = useState({ email: "", password: "" });
-  const [error, setError] = useState(null);
-
-  // Initialize auth instance
-  const auth = getAuth();
-
-  // Handle form submit
-  const handleLogin = (e) => {
-    e.preventDefault();
-    setError("");
-    const email = input.email.toLocaleLowerCase().trim();
-    const password = input.password;
-
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Sign in
-        console.log(userCredential.user);
-        // ...
-      })
-      .catch((error) => {
-        if (
-          error.code === AuthErrorCodes.INVALID_PASSWORD ||
-          error.code === AuthErrorCodes.USER_DELETED
-        ) {
-          setError("The email or password is incorrect");
-        } else {
-          console.log(error.code);
-          alert(error.code);
-        }
-      });
-  };
-
 
   return (
     <div>
     <div className="profile-container">
       <h2 className="login mb-4 mt-4 ">Login to Your Account</h2>
-      <form onSubmit={handleLogin} className="login-form">
+      <form className="login-form">
         <div className="mb-4">
           <label htmlFor="email" className="form-label">
             Username:
@@ -47,8 +14,6 @@ function LoginForm() {
           <input
             type="text"
             id="email"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
             className="form-control"
             required
             placeholder='Your email'
@@ -61,8 +26,6 @@ function LoginForm() {
           <input
             type="password"
             id="password"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
             className="form-control"
             required
             placeholder='Password'
