a064c57d9f84d3b1177e224b5526ff64aa11c254
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-04-11 02:43:58 +0200

    created signup file to handle firebase create user account logic

---
+++ src/Components/GoogleSignIn/Signup.jsx
@@ -0,0 +1,44 @@
+import { createUserWithEmailAndPassword} from 'firebase/auth';
+import React, {useState} from 'react';
+import { auth } from './Firebase';
+
+//initialised user email and password
+const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    //function handle sign in with user credentials and prevent loading and submit event 
+
+    const signUp = (e) => {
+       e.preventDefault();
+       createUserWithEmailAndPassword(auth, email, password)
+       .then((userCredential) => {
+        console.log(userCredential)
+       }).catch((error) =>  {
+        console.log(error);
+       })
+    }
+
+    //created form to display  input fields for email and password
+  return (
+     <div className='sign-in-container'>
+      <form onSubmit={signUp}>
+      <h2>Create account</h2>
+      <input 
+      type="email" 
+      placeholder='Enter your email' 
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      ></input>
+      <input type="password" 
+      placeholder='Enter your password' 
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      ></input>
+        <button type="submit"> signUp </button>
+      </form>
+    </div>
+  )
+}
+
+export default SignUp
