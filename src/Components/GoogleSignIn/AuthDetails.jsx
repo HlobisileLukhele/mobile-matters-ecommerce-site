56ce895f58b7d86cbd037d7aad0efd4a67e44fbf
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-04-11 02:45:31 +0200

    created file to display users when signed in and created a button to log out

---
+++ src/Components/GoogleSignIn/AuthDetails.jsx
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from 'react'
+import {auth}  from './Firebase';
+import { onAuthStateChanged,signOut } from 'firebase/auth';
+
+//initialize authUser
+const AuthDetails = () => {
+    const [authUser, setAuthUser] = useState(null);
+
+    useEffect(() => {
+        const listen = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setAuthUser(user)
+            } else {
+                setAuthUser(null);
+            }
+        });
+
+        return () => {
+            listen();
+        }
+    }, []);
+
+    //declare signout function, console log and catch error
+    const userSignOut = () => {
+        signOut(auth).then(() => {
+            console.log('signed out successful')
+        }).catch(error => console.log(error))
+    }
+//display the auth user when signed in
+  return (
+    <div>
+      { authUser ? <><p>{`Signed In as ${authUser.email}`} </p><button onClick={userSignOut}>Sign Out</button></> : <p>Signed Out </p>}
+    </div>
+  )
+}
+
+export default AuthDetails
