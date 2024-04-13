522bd74c7d47828d13642373d78251287c74a70a
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-04-13 02:48:56 +0200

    remoed signin import

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import SignIn from "./GoogleSignIn/SignIn";
 import SignUp from './GoogleSignIn/Signup';
 import AuthDetails from './GoogleSignIn/AuthDetails';
 
@@ -7,7 +6,7 @@ const Profile = () => {
   return (
     <div className="profile-container">
     <SignUp />
-    <SignIn />
+    <br />
     <AuthDetails />
     </div>
   );
