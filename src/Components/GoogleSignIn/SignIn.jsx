eba677069909290b048c09ee621ddd6c305582aa
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-04-13 02:46:39 +0200

    added id to link form and form control,  created paragraph to sign in if dont have an account

--- src/Components/GoogleSignIn/SignIn.jsx
+++ src/Components/GoogleSignIn/SignIn.jsx
@@ -22,21 +22,33 @@ const SignIn = () => {
 
   return (
      <div className='sign-in-container'>
-      <form onSubmit={signIn}>
-      <h2>Log in</h2>
+      <form onSubmit={signIn} id="signin">
+      <h2 className="login mt-5 pb-4">Log in To your Account </h2>
+      <label htmlFor="email" className="form-label font-medium"> Email Address: </label>
       <input 
-      type="email" 
+      className='form-control ' 
+      type="text"
+      id="email" 
       placeholder='Enter your email' 
       value={email}
       onChange={(e) => setEmail(e.target.value)}
       ></input>
-      <input type="password" 
+      <label htmlFor="email" className="form-label font-medium"> Password: </label>
+      <input 
+      className='form-control ' 
+      type="password" 
+      id="password"
       placeholder='Enter your password' 
       value={password}
       onChange={(e) => setPassword(e.target.value)}
       ></input>
-        <button type="submit"> LOGIN </button>
+      <span className="signin-link d-flex align-right  bg-gray-600 mb-3 mt-2 "><i>Forgot Password</i></span>
+      <button type="submit btn btn-primary border-radius-4 pt-2" className='btn btn-primary'> Sign In </button>
+    
       </form>
+      <p className='mt-4 text-center'>Don't have an account?</p> 
+      <p><a href="Signup" className="font-weight-bold pb-2 "><u>Sign Up</u></a></p>
+      <hr />
     </div>
   )
 }
