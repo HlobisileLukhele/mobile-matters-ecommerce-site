4b7f27e2416ba0c7ae79da19ca5a6f52057f1d80
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-04-13 02:48:32 +0200

    added id to link the signin form and added form control and required , and login link if user already have ana ccount

--- src/Components/GoogleSignIn/Signup.jsx
+++ src/Components/GoogleSignIn/Signup.jsx
@@ -2,12 +2,12 @@ import { createUserWithEmailAndPassword} from 'firebase/auth';
 import React, {useState} from 'react';
 import { auth } from './Firebase';
 
-//initialised user email and password
-const SignUp = () => {
+  //initialised user email and password
+  const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    //function handle sign in with user credentials and prevent loading and submit event 
+  //function handle sign in with user credentials and prevent loading and submit event 
 
     const signUp = (e) => {
        e.preventDefault();
@@ -22,21 +22,39 @@ const SignUp = () => {
     //created form to display  input fields for email and password
   return (
      <div className='sign-in-container'>
-      <form onSubmit={signUp}>
-      <h2>Create Account</h2>
+      <form onSubmit={signUp} id="signup">
+      <h2 className="create-account pb-3 font-serif ">Create Account</h2> 
+      <label htmlFor="email" className="form-label font-medium "> Email Address : </label>
       <input 
-      type="email" 
+      required
+      className='form-control' 
+      type="text" 
+      id="email"
       placeholder='Enter your email' 
       value={email}
       onChange={(e) => setEmail(e.target.value)}
       ></input>
-      <input type="password" 
+      <label htmlFor="email" className="form-label font-medium"> Password: </label>
+      <input 
+      className='form-control ' 
+      type="password"
+      id="password"
+      required
       placeholder='Enter your password' 
       value={password}
       onChange={(e) => setPassword(e.target.value)}
-      ></input>
-        <button type="submit"> Sign Up </button>
+      >
+      </input>
+      <span className="signin-link d-flex align-right pb-2 pl-2  ">
+      Already have an account,  
+      <a href="signin"> <i> login</i></a>
+      </span>
+      <button type="submit" className="btn btn-primary mt-3 mb-2 font-medium "> Sign Up </button>
+      <br />
+      <button type="button" class="login-with-google-btn mt-1 mb-4 ">Login with Google</button>
+
       </form>
+      <hr />
     </div>
   )
 }
