7070ec915248775e35c02a1a76d6123249844456
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-04-15 20:03:08 +0200

    removed categories import file

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Categories from './Categories';
 import Footer from './Footer'
+import FeatureProductsList from "./FeatureProductsList"
 
 const Home = () => (
   <div className="Container-fluid mb-4">
@@ -8,8 +8,8 @@ const Home = () => (
     <h1 className="title is-1 text-center mt-6 mb-6">Your One Stop Tech Store</h1>
     <img src="images/apple.png" className="img-fluid landing-page-img mb-4 align-items-center" alt="apple iphones front"/>
   </div>
-    <Categories />
-    <Footer />
+  <FeatureProductsList />
+  <Footer />
   </div>
 );
 export default Home;
