02bce0557932208de1e2d03b5e32d2337d903605
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-04-23 16:47:42 +0200

    remoed rendered store

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 import {Provider } from 'react-redux';
-import { store } from './Features/Store.jsx';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 
 const root = createRoot(document.getElementById('root'));
 root.render(    
-<Provider store={store}>
+<Provider >
   <StrictMode>
     <App />
  </StrictMode>
