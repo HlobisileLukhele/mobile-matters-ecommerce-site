833f65a4b6c69d6fce99d8ebcdea381483e4e171
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-04-26 00:30:24 +0200

    import featuredprouctslist and rendered the file

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Footer from './Footer'
+import FeaturedProductsList from './FeaturedProductList'
 
 const Home = () => (
   <div className="Container-fluid mb-4">
@@ -7,6 +8,7 @@ const Home = () => (
     <h1 className="title is-1 text-center mt-6 mb-6">Your One Stop Tech Store</h1>
     <img src="images/apple.png" className="img-fluid landing-page-img mb-4 align-items-center" alt="apple iphones front"/>
   </div>
+  <FeaturedProductsList />
   <Footer />
   </div>
 );
