f380fcbfe2fa5e32262ea9fa4c83726eb37cdb36
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-04-25 23:37:58 +0200

    removed provider import

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
-import {Provider } from 'react-redux';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 
 const root = createRoot(document.getElementById('root'));
 root.render(    
-<Provider >
   <StrictMode>
     <App />
  </StrictMode>
-</Provider>
 );
     
