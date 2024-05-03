7f83b82d7ae05c156c7901468978af84d0f867f0
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-03 19:37:08 +0200

    removed unused stylings

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,7 +3,12 @@ import ContactForm from "./ContactForm"
 import Footer from "./Footer"
 
 const Contact = () => (
-  <div>  
+  <div className="container border-x-2 border-y-2">  
+      <ul className="headings">
+        <li className="active:first-line: login">Login</li> 
+        <li className="active:first-line: register">Register</li> 
+
+      </ul>
     <ContactForm /> 
     <Footer />
   </div>
