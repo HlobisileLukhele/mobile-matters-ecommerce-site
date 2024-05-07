04fb99bb173b9557539c8b8db3404b0ec8d9a1e6
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-05-07 18:31:07 +0200

    removed the heading list

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,12 +3,7 @@ import ContactForm from "./ContactForm"
 import Footer from "./Footer"
 
 const Contact = () => (
-  <div className="container border-x-2 border-y-2">  
-      <ul className="headings">
-        <li className="active:first-line: login">Login</li> 
-        <li className="active:first-line: register">Register</li> 
-
-      </ul>
+  <div className="container border-x-2 border-y-2 mt-4 ">  
     <ContactForm /> 
     <Footer />
   </div>
